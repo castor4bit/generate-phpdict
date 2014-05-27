15ad15d7801ae50d2a0d1c98193dc8b85dd90f8f
castor.4bit <castor.4bit@gmail.com>
2014-05-27 11:57:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## PHP.dict
+
+Generate PHP.dictfor vim.
+
+## Deploying
+
+Install the [Heroku Toolbelt](https://toolbelt.heroku.com/).
+
+```sh
+$ git clone git@github.com:castor4bit/generate-phpdict.git
+$ cd generate-phpdict
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
---
+++ web/index.php
@@ -0,0 +1,27 @@
+<?php
+// http://www.asahi-net.or.jp/~wv7y-kmr/memo/vim_php.html
+$keywords = array(
+    'if', 'else', 'elseif', 'endif', 'while', 'endwhile', 'do', 'as', 'for', 'endfor', 'foreach', 'endforeach',
+    'break', 'continue', 'switch', 'endswitch', 'case', 'default', 'declare', 'enddeclare',
+    'try', 'catch', 'return', 'exit', 'const', 'class', 'function',
+    'require', 'include', 'require_once', 'include_once',
+    'abstract', 'final', 'interface', 'private', 'protected', 'public', 'static',
+    '__LINE__', '__FILE__', '__DIR__', '__FUNCTION__', '__CLASS__', '__METHOD__', '__NAMESPACE__'
+);
+$functions  = function_exists( 'get_defined_functions'   ) ? get_defined_functions()   : array();
+$constants  = function_exists( 'get_defined_constants'   ) ? get_defined_constants()   : array();
+$interfaces = function_exists( 'get_declared_interfaces' ) ? get_declared_interfaces() : array();
+$classes    = function_exists( 'get_declared_classes'    ) ? get_declared_classes()    : array();
+
+$arrays = array_merge(
+    $keywords,
+    $functions['internal'],
+    array_keys( $constants ),
+    $interfaces,
+    $classes
+);
+sort( $arrays );
+$arrays = array_unique( $arrays );
+
+header('Content-Type: text/plain');
+echo implode( "\n", $arrays );
